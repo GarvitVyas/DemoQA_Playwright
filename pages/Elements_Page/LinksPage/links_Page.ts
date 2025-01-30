2acf50141022d9678d9a8062469623115693f964
Garvit Vyas <respondtogarvit@gmail.com>
2025-01-30 23:43:24 +0530

    added a new test for home link
    added new method for homeLink in page class

--- pages/Elements_Page/LinksPage/links_Page.ts
+++ pages/Elements_Page/LinksPage/links_Page.ts
@@ -4,13 +4,20 @@ import { ElementActions } from '../../data/utils/action_utils';
 class Links{
     private page:Page;
     private elementActions:ElementActions;
+    private homeSimpleLink:string;
     constructor(page:Page){
         this.page = page;
+        this.homeSimpleLink='a#simpleLink';
         this.elementActions = new ElementActions();
     }
 
     async verifyLinksPage(){
         return await this.page.url();
+    }
+
+    async verifyHomeSimpleLink(){
+        const value = await this.page.locator(this.homeSimpleLink).innerText();
+        return value;
     }
 }
 export{Links}
--- tests/links.spec.ts
+++ tests/links.spec.ts
@@ -1,10 +1,12 @@
 import {test, expect} from '@playwright/test';
 import { Element } from '../pages/Elements_Page/navigate_to_element';
 import { Links } from '../pages/Elements_Page/LinksPage/links_Page';
+import { data } from '../pages/data/data';
 
 test.describe('@linksPage - tests to verify links page',()=>{
     let elementPage:Element;
     let linksPage:Links;
+
 
     test.beforeEach(async({page})=>{
         try{
@@ -15,6 +17,16 @@ test.describe('@linksPage - tests to verify links page',()=>{
             console.info('Issue with before each hook for links test');
             throw err;
         }
+    })
+
+    test('@homeLink - verify the home link opens new tab',async({})=>{
+        await elementPage.navigateToElementPage();
+        await elementPage.navigateToLinksPage();
+        expect(await linksPage.verifyLinksPage()).toContain(data['links page']);
+
+        expect(await linksPage.verifyHomeSimpleLink()).toBe('Home');
+        
+
     })
 
     
