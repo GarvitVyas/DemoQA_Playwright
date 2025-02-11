78062bc0932bdda990fd8f90edd08f32b783a91a
Garvit Vyas <respondtogarvit@gmail.com>
2025-02-11 23:18:14 +0530

    added method for links opening up new page with destructuring and without it

--- pages/Elements_Page/LinksPage/links_Page.ts
+++ pages/Elements_Page/LinksPage/links_Page.ts
@@ -5,9 +5,11 @@ class Links{
     private page:Page;
     private elementActions:ElementActions;
     private homeSimpleLink:string;
+    private homeDynamicLink:string;
     constructor(page:Page){
         this.page = page;
         this.homeSimpleLink='a#simpleLink';
+        this.homeDynamicLink='a#dynamicLink'
         this.elementActions = new ElementActions();
     }
 
@@ -20,18 +22,37 @@ class Links{
         return value;
     }
 
-    async actionLink(){
+    async actionHomeLink(){
         const homeLink = await this.page.locator(this.homeSimpleLink);
-       // await homeLink.click();
         const [newPage] = await Promise.all([
              this.page.context().waitForEvent('page'),
               homeLink.click(),            
         ]);
-
+        /**
+         * here, if you dont use const [newPage] destructuring assignment you will get
+         * a array from Promise.all([..]) and then you need to access the page object
+         * return by the first statement in Promise.all([])
+         * using it's index like newPage[0].
+         */
         await newPage.waitForLoadState();
         const newUrl = await newPage.url();
         await newPage.close();
         return newUrl;
+    }
+
+    async verifyHomeDynamicLink(){
+        const value = await this.page.locator(this.homeDynamicLink).innerText();
+        return value;
+    }
+
+    async actionHomeDynamicLink(){
+        const homedynamic = await this.page.locator(this.homeDynamicLink);
+        const result = await Promise.all([
+            this.page.context().waitForEvent('page'),
+            homedynamic.click()
+        ])
+        const newPage = await result[0];
+        return await newPage.url();
     }
 }
 export{Links}
