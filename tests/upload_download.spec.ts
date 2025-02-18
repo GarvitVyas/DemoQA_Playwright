a84ffeca1e989a97006e45e5bb243e0750d0da23
Garvit Vyas <respondtogarvit@gmail.com>
2025-02-19 00:50:47 +0530

    updated it as per the custom fixture

--- tests/upload_download.spec.ts
+++ tests/upload_download.spec.ts
@@ -1,24 +1,24 @@
-import {test,expect} from '@playwright/test';
-import { Element } from '../pages/Elements_Page/navigate_to_element';
-import { UploadDownload } from '../pages/Elements_Page/UploadDownload/upload_download';
+import {test,expect} from '../base';
+//import { Element } from '../pages/Elements_Page/navigate_to_element';
+//import { UploadDownload } from '../pages/Elements_Page/UploadDownload/upload_download';
 import { data} from '../pages/data/data';
 
 test.describe('@uploaddownload - tests for upload and download page',()=>{
-    let elementPage:Element;
-    let uploadDownloadPage: UploadDownload;
+   // let elementPage:Element;
+   // let uploadDownloadPage: UploadDownload;
 
     test.beforeEach(async({page})=>{
         try{
             await page.goto('/');
-            elementPage = new Element(page);
-            uploadDownloadPage = new UploadDownload(page);
+           // elementPage = new Element(page);
+            //uploadDownloadPage = new UploadDownload(page);
         }catch(err){
             console.info('Issue with before each hook for upload download page');
             throw err;
         }
     })
 
-    test('@uploadImage - upload and verify uploaded',async({})=>{
+    test('@uploadImage - upload and verify uploaded',async({elementPage, uploadDownloadPage})=>{
         await elementPage.navigateToElementPage();
         await elementPage.navigateToUploadDownload();
         expect(await uploadDownloadPage.verifyUploadDownload()).toContain(data['upload download']);
@@ -33,7 +33,7 @@ test.describe('@uploaddownload - tests for upload and download page',()=>{
         expect(await uploadDownloadPage.uploadPath()).toContain('download.png');     
     })
 
-    test('@downloadImage - download image and verify downloaded',async({})=>{
+    test('@downloadImage - download image and verify downloaded',async({elementPage,uploadDownloadPage})=>{
         await elementPage.navigateToElementPage();
         await elementPage.navigateToUploadDownload();
         expect(await uploadDownloadPage.verifyUploadDownload()).toContain(data['upload download']);
