8cdaa48d18634e8edcb14ac1df0da5a939ec0433
Garvit Vyas <respondtogarvit@gmail.com>
2025-02-22 17:30:42 +0530

    removed page initialization statements

--- tests/upload_download.spec.ts
+++ tests/upload_download.spec.ts
@@ -1,17 +1,11 @@
 import {test,expect} from '../base';
-//import { Element } from '../pages/Elements_Page/navigate_to_element';
-//import { UploadDownload } from '../pages/Elements_Page/UploadDownload/upload_download';
 import { data} from '../pages/data/data';
 
 test.describe('@uploaddownload - tests for upload and download page',()=>{
-   // let elementPage:Element;
-   // let uploadDownloadPage: UploadDownload;
 
     test.beforeEach(async({page})=>{
         try{
             await page.goto('/');
-           // elementPage = new Element(page);
-            //uploadDownloadPage = new UploadDownload(page);
         }catch(err){
             console.info('Issue with before each hook for upload download page');
             throw err;
