e46fb1519efded95601167998ac2a3f10174ff9b
Garvit Vyas <respondtogarvit@gmail.com>
2025-02-23 22:34:18 +0530

    added test for validating the fields in the practice form

--- tests/practice_forms.spec.ts
+++ tests/practice_forms.spec.ts
@@ -6,12 +6,41 @@ test.describe('@PracticeForm - test to verify the practice automation form',()=>
 
     test.beforeEach(async({page})=>{
         try{
-            await page.goto('/');
+            await page.goto('/',{waitUntil:'domcontentloaded'});
+            //await page.waitForLoadState('load');
         }catch(err){
+            console.error('Error during page load: ', err);
             throw new Error('Issue with before each block of practice form class');
         }
     })
 
-    
+    test('@validateFields - validate form fields',async({formsPage,practiceFormPage})=>{
+        await formsPage.navigateToForms();
+        await formsPage.navigateToPracticeFormsPage();
+
+        expect(await practiceFormPage.verifyPracticeForm()).toContain(data['practice form page']);
+        expect(await practiceFormPage.verifyFormTitle()).toBe(data['froms title']);
+
+        const fields = await practiceFormPage.verifyFeilds();
+        await expect(fields['First name']).toHaveAttribute('placeholder','First Name');
+        await expect(fields['Last name']).toHaveAttribute('placeholder','Last Name');
+        await expect(fields['email']).toHaveAttribute('placeholder','name@example.com');
+        await expect(fields['male_gender']).toHaveValue('Male');
+        await expect(fields['female_gender']).toHaveValue('Female');
+        await expect(fields['other_gender']).toHaveValue('Other');
+        await expect(fields['mobile']).toHaveAttribute('placeholder','Mobile Number');
+        //verify dob value as current date
+        const currentDate = await practiceFormPage.currentDate();
+        await expect(fields['date of birth']).toHaveValue(currentDate);
+        //hobbies
+        expect(await fields['sport_hobbie'].innerText()).toBe('Sports');
+        expect(await fields['reading_hobbie'].innerText()).toBe('Reading');
+        expect(await fields['music_hobbie'].innerText()).toBe('Music');
+        //current address
+        await expect(fields['current_address']).toHaveAttribute('placeholder','Current Address');
+        //state
+        expect(await fields['state'].innerText()).toBe('Select State');
+        expect(await fields['city'].innerText()).toBe('Select City');
+    })
 
 })
