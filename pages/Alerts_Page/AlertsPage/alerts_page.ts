a85ee1dc94a0b85c47388bd7342be59b339a649c
Garvit Vyas <respondtogarvit@gmail.com>
2025-03-04 23:24:06 +0530

    added methods for confirming and dismissing the confimation pop up

--- pages/Alerts_Page/AlertsPage/alerts_page.ts
+++ pages/Alerts_Page/AlertsPage/alerts_page.ts
@@ -1,12 +1,15 @@
-import {Page} from '@playwright/test';
+import {Dialog, Page} from '@playwright/test';
 
 class Alerts{
     private page:Page;
     private falert:string;
     private salert:string;
-
+    private talert:string;
+    private tresult :string;
     constructor(page:Page){
         this.page = page;
+        this.tresult='//*[@id="confirmResult"]';
+        this.talert='//*[@id="confirmButton"]';
         this.falert='//*[@id="alertButton"]';
         this.salert='//*[@id="timerAlertButton"]';
     }
@@ -33,6 +36,44 @@ class Alerts{
         await dialog.accept();
 
         return [await dialog.message(), await dialog.type()];
+    }
+
+
+    async confirmThirdAlert(){
+       return new Promise<{msg:string,type:string,result:string}>((resolve,reject)=>{
+         this.page.once('dialog',async(res)=>{
+          try{
+            const dialog = res; 
+            dialog.accept();
+            let msg = await dialog.message();
+            let type = await dialog.type();
+            let result = await this.page.locator(this.tresult).innerText();
+            resolve({msg,type,result});
+          }catch(err){
+            reject(err);
+          }
+         })
+          this.page.locator(this.talert).click();
+       })
+   
+    }
+
+    async dismisThirdAlert(){
+        return new Promise<{msg:string,type:string,result:string}>((resolve,reject)=>{
+            this.page.on('dialog',async(res)=>{
+                try{
+                    res.dismiss();
+                    const msg = await res.message();
+                    const type = await res.type();
+                    const result = await this.page.locator(this.tresult).innerText();
+                    resolve({msg,type,result});
+                }catch(err){
+                    reject(err);
+                }
+            })
+            
+            this.page.locator(this.talert).click();
+        })
     }
      
     
