480255a937b37ab0fcc8dd95896829c2633d1f69
Garvit Vyas <respondtogarvit@gmail.com>
2025-03-08 23:14:05 +0530

    added message for small and large modal and url validation

--- pages/data/data.ts
+++ pages/data/data.ts
@@ -25,6 +25,9 @@ const data={
     'sample-page':'This is a sample page',
     'frames page':'frames',
     'nested frames':'nestedframes',
+    'modal dialogs':'modal-dialogs',
+    'small modal message':'This is a small modal. It has very less content',
+    'large modal message':"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
     'new window message':'Knowledge increases by sharing but not by saving. Please share this website with your friends and in your organization.'
 };
 
