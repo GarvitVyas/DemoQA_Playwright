81596a8b23c78aeb2c6883e9f43e9c4a7c3ac4b1
Garvit Vyas <respondtogarvit@gmail.com>
2025-03-19 23:32:26 +0530

    added text validation values for accordian page

--- pages/data/data.ts
+++ pages/data/data.ts
@@ -28,7 +28,11 @@ const data={
     'modal dialogs':'modal-dialogs',
     'small modal message':'This is a small modal. It has very less content',
     'large modal message':"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-    'new window message':'Knowledge increases by sharing but not by saving. Please share this website with your friends and in your organization.'
+    'new window message':'Knowledge increases by sharing but not by saving. Please share this website with your friends and in your organization.',
+    'accordian page':'accordian',
+    'first accordian heading':'What is Lorem Ipsum?',
+    'second accordian heading':'Where does it come from?',
+    'third accordian heading':'Why do we use it?'
 };
 
 const staticUser={
