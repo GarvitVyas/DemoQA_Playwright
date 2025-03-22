a37feeda7543e78c641278d9517ae45c8405e317
Garvit Vyas <respondtogarvit@gmail.com>
2025-03-22 20:33:44 +0530

    added page class for auto complete page

---
+++ pages/Widgets_Page/AutoComplete/autoComplete.ts
@@ -0,0 +1,71 @@
+import {Page} from '@playwright/test';
+import { ElementActions } from '../../data/utils/action_utils';
+
+class AutoComplete{
+
+    private page:Page;
+    private elementAction:ElementActions;
+    private firstHeading:string;
+    private secondHeading:string;
+    private multipleinput:string;
+    private multiplevalue:string;
+    private clearsingle : string;
+    private clearall:string;
+
+    constructor(page:Page){
+        this.page = page;
+        this.elementAction = new ElementActions;
+        this.clearall='//div[contains(@class,"clear-indicator")]';
+        this.multiplevalue='//div[contains(@class,"css-12jo7m5")]';
+        this.firstHeading='//*[@id="autoCompleteMultiple"]/span';
+        this.secondHeading='//*[@id="autoCompleteSingle"]/span';
+        this.multipleinput='//*[@id="autoCompleteMultipleInput"]';
+        this.clearsingle='//div[contains(@class,"multi-value__remove")]'
+    }
+
+    async verifyAutoCompletePage():Promise<string>{
+        return await this.page.url();
+    }
+
+    async fieldHeadings():Promise<[heading1?:string,heading2?:string]>{
+        const heading1 = await this.elementAction.returnText(await this.page.locator(this.firstHeading));
+        const heading2 = await this.elementAction.returnText(await this.page.locator(this.secondHeading));
+        return [heading1,heading2];
+    }
+
+    async multipleInputField(){
+        await this.elementAction.fillInData(await this.page.locator(this.multipleinput),'r');
+        await this.page.getByText('Green').click();
+        await this.page.locator(this.multipleinput).fill('r')
+        await this.page.getByText('Red').click();
+    }
+
+    async verifyMultipleInputField(){
+        const values = await this.page.locator(this.multiplevalue).all();
+        return values;
+    }
+
+    async clearSelected(){
+        const flag = await this.page.getByText('Green').isVisible();
+        if(flag){
+            await this.page.locator(this.clearsingle).first().click();
+        }
+    }
+
+    async clearAll(){
+        try{
+            const flag = await this.page.locator(this.clearall).isVisible();
+            if(flag){
+                await this.page.locator(this.clearall).click();
+            }else{
+                throw new Error('Clear all button is not visible');
+            }
+        }catch(err){
+            console.log('Issue with clear all button',err)
+        }
+    }
+
+
+
+
+}export{AutoComplete}
