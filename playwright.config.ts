662d01f39aa999162f3ba1f99b29dbd3b1d5ef0e
Garvit Vyas <respondtogarvit@gmail.com>
2025-03-23 20:14:03 +0530

    updated  project properties

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,7 +12,7 @@ import {config} from './config';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  testDir: './tests',
+  /*testDir: './tests',*/
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
@@ -29,7 +29,6 @@ export default defineConfig({
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
      baseURL: config.baseURL,
-
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
     screenshot:'only-on-failure',
@@ -44,6 +43,7 @@ export default defineConfig({
   projects: [
     {
       name: 'chromium',
+      testDir:'tests',
       use: { ...devices['Desktop Chrome'] },
     },
 
