c77572448c756a3abf4fb744d5f1a1b68a029207
Garvit Vyas <respondtogarvit@gmail.com>
2025-03-24 23:19:03 +0530

    added test spec for slider page

---
+++ tests/slider.spec.ts
@@ -0,0 +1,22 @@
+import {test,expect} from '../base';
+import { data } from '../pages/data/data';
+
+test.describe('@slider - verify the slider functionality',()=>{
+
+    test.beforeEach(async({page})=>{
+        try{
+            await page.goto('/');
+        }catch(err){
+            console.log('Issue with before each hook of slider page',err);
+        }
+    })
+
+    test('@firstSliderTest - verify the slider initial values',async({widgetsPage,sliderPage})=>{
+        await widgetsPage.naivigateToWidgetsPage();
+        await widgetsPage.navigateToSlider();
+        expect(await sliderPage.verifySliderPage()).toBe(data['slider page']);
+
+    })
+
+
+})
