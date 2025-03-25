450d8a166e43947f2d2562f2e4fca1c806967d57
Garvit Vyas <respondtogarvit@gmail.com>
2025-03-25 22:48:40 +0530

    added method for slider initial value

--- pages/Widgets_Page/Slider/slider.ts
+++ pages/Widgets_Page/Slider/slider.ts
@@ -3,13 +3,27 @@ import {Page} from '@playwright/test';
 class Slider{
 
     private page:Page;
-
+    private slidervalueField:string;
+    private inputrange:string;
     constructor(page:Page){
         this.page = page;
+        this.inputrange='//input[@type="range"]'
+        this.slidervalueField='//input[@id="sliderValue"]';
     }
 
     async verifySliderPage(){
         return await this.page.url();
+    }
+
+    async sliderValue(){
+        return await this.page.locator(this.slidervalueField).getAttribute('value');
+    }
+
+    async hoverOverSlider(){
+        const ele = await this.page.locator(this.inputrange);
+        await ele.hover();
+        const flag = await this.page.getByText('25').isVisible();
+        return flag;
     }
 
 }export{Slider}
