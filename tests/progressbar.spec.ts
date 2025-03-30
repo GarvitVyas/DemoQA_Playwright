e92e3817bf05ba83878ff7b00fc0d50117584a10
Garvit Vyas <respondtogarvit@gmail.com>
2025-03-30 23:33:11 +0530

    added tests for progress bar move, button text, and reset button

--- tests/progressbar.spec.ts
+++ tests/progressbar.spec.ts
@@ -10,10 +10,35 @@ test.describe('@progressBar - verify the progress bar functionality',()=>{
         }
     })
 
-    test('@',async({widgetsPage,progressbarPage})=>{
+    test('@moveBar - move the bar to specific value 85%',async({widgetsPage,progressbarPage})=>{
         await widgetsPage.naivigateToWidgetsPage();
         await widgetsPage.navigateToPorgressBar();
         expect(await progressbarPage.verifyProgressBarPage()).toContain(data['progress bar']);
+
+        await progressbarPage.moveProgress();
+        expect(await progressbarPage.barValue()).toBe('85%');
+    })
+
+    test('@startStopText - verify the text on button when start and stop',async({widgetsPage,progressbarPage})=>{
+        await widgetsPage.naivigateToWidgetsPage();
+        await widgetsPage.navigateToPorgressBar();
+        expect(await progressbarPage.verifyProgressBarPage()).toContain(data['progress bar']);
+
+        const [initial,middle] = await progressbarPage.startStopButtonValidation();
+        expect(initial).toBe('Start');
+        expect(middle).toBe('Stop');
+
+        expect(await progressbarPage.resetButton()).toBe(true);
+    })
+
+    test('@barColor - verify the bar color on start and reset',async({widgetsPage,progressbarPage})=>{
+        await widgetsPage.naivigateToWidgetsPage();
+        await widgetsPage.navigateToPorgressBar();
+        expect(await progressbarPage.verifyProgressBarPage()).toContain(data['progress bar']);
+
+        const [initial,final]= await progressbarPage.barCSS();
+        expect(initial).toBe('rgb(23, 162, 184)');
+        expect(final).toBe('rgb(40, 167, 69)') 
     })
 
 })
