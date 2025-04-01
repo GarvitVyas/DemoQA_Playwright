ab965eaecf60308dd7ffe01ad7719ce50e2ff11f
Garvit Vyas <respondtogarvit@gmail.com>
2025-04-02 00:37:44 +0530

    added tests for input, contrary, geolocation tooltip

--- tests/toolTip.spec.ts
+++ tests/toolTip.spec.ts
@@ -11,10 +11,41 @@ test.describe('@toolTip - verify the tool tips functionality',()=>{
         }
     })
 
-    test('@tooltip - verify the first tooltip functionality',async({widgetsPage,tooltipPage})=>{
+    test('@buttontooltip - verify the first tooltip functionality',async({widgetsPage,tooltipPage})=>{
         await widgetsPage.naivigateToWidgetsPage();
         await widgetsPage.navigateToToolTip();
         expect(await tooltipPage.verifyToolTipPage()).toContain(data['tool tip page']);
+
+        const message = await tooltipPage.buttonToolTipText();
+        expect(await message).toBe('You hovered over the Button');
+
     })
+
+   test('@inputtooltip - verify the input tool tip functionality',async({widgetsPage,tooltipPage})=>{
+        await widgetsPage.naivigateToWidgetsPage();
+        await widgetsPage.navigateToToolTip();
+        expect(await tooltipPage.verifyToolTipPage()).toContain(data['tool tip page']);
+        const message2 = await tooltipPage.inputToolTipText();
+        expect(await message2).toBe('You hovered over the text field');
+   })
+
+   test('@contrarytooltip - verify the contrary tool tip functionality',async({widgetsPage,tooltipPage})=>{
+    await widgetsPage.naivigateToWidgetsPage();
+    await widgetsPage.navigateToToolTip();
+    expect(await tooltipPage.verifyToolTipPage()).toContain(data['tool tip page']);
+    const message2 = await tooltipPage.contraryToolTipText();
+    expect(await message2).toBe('You hovered over the Contrary');
+    })
+
+    test('@geolocationtooltip - verify the geolocation tool tip functionality',async({widgetsPage,tooltipPage})=>{
+        await widgetsPage.naivigateToWidgetsPage();
+        await widgetsPage.navigateToToolTip();
+        expect(await tooltipPage.verifyToolTipPage()).toContain(data['tool tip page']);
+        const message2 = await tooltipPage.geoToolTipText();
+        expect(await message2).toBe('You hovered over the 1.10.32');
+    })
+
+
+
 
 })
